1c8206660fc5193a32f8cef2892c0e4dbd68884c
Candela Leguizamon <candelegui02@outlook.com>
2026-06-08 14:12:14 -0300

    test: add infrastructure migration tests

---
+++ src/test/java/com/turnero/api/integration/ContextLoadTest.java
@@ -0,0 +1,13 @@
+package com.turnero.api.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class ContextLoadTest {
+    @Test
+    void contextLoads() {
+    }
+}
